3ab0a71945376ea4742f2a37855948c427ef90c8
Adeola-creator <111973534+deeygitalstudio@users.noreply.github.com>
2023-11-07 00:26:19 +0100

    Your commit

---
+++ src/Component/NewsBoard.jsx
@@ -0,0 +1,28 @@
+import  { useState, useEffect } from 'react'
+import NewsItem from '../Component/NewsItem'
+
+const NewsBoard = ({category}) => {
+
+    const [articles, setArticles] = useState([])
+
+  useEffect(() => {
+let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${process.env.REACT_APP_API_KEY}`;
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            setArticles(data.articles);
+            console.log(data.articles); // Log the articles after setting state
+        });
+}, [category]);
+
+  return (
+    <div className='container-fluid row'>
+      <h1 className='text-center mt-4'>Breaking <span className='badge bg-danger'>News</span></h1>
+      {articles.map((news, index) => {
+        return <NewsItem key={index} title={news.title} description={news.description} src={news.urlToImage} url={news.url} />
+      })}
+    </div>
+  )
+}
+
+export default NewsBoard
