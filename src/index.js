5d58f9e1af40bcc5cb845941ed69d8d500d407b9
Akinbohun Ifedayo <111973534+deeygitalstudio@users.noreply.github.com>
2025-06-12 23:40:19 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import { HashRouter } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <HashRouter>
-       <App />
-   </HashRouter>
+ <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
